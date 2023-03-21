14f72e6f372de521c49d4579592b42bd0a3d2157
Ayrton Gonçalves Guerra <ayrtonguerra@pop-os.localdomain>
2023-03-21 13:39:26 -0300

    feat(utils): setting data and types of the form address and setting style in render test

---
+++ src/types/form/FormAddress.ts
@@ -0,0 +1,9 @@
+export type FormAddressProps = {
+  zipcode: string;
+  street: string;
+  complement: string;
+  number: string;
+  district: string;
+  city: string;
+  state: string;
+};
---
+++ src/utils/baseData/FormAddress.ts
@@ -0,0 +1,11 @@
+import { FormAddressProps } from '../../types/form/FormAddress';
+
+export const FormAddressData: FormAddressProps = {
+  zipcode: '',
+  number: '',
+  complement: '',
+  district: '',
+  city: '',
+  state: '',
+  street: '',
+};
