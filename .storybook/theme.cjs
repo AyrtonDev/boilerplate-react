e561b9539099344344ec29b4b1805602b0c72176
Ayrton Gonçalves Guerra <ayrtonguerra@pop-os.localdomain>
2023-03-24 09:35:50 -0300

    chore(story): change files config and creating others to condig

---
+++ .storybook/theme.cjs
@@ -0,0 +1,10 @@
+import { create, themes } from '@storybook/theming';
+
+export default create({
+  base: themes.dark,
+
+  colorPrimary: '#ffffff',
+  colorSecondary: '#B80561',
+
+  brandTitle: 'Boilerplate react',
+});
